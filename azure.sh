5fb3fa2380550df3f6e8efa258c97d6aa10685c0
Evan Allen <evanallen13@github.com>
2025-08-19 11:03:40 -0400

    Create azure.sh

---
+++ azure.sh
@@ -0,0 +1,21 @@
+# Variables – tweak as needed
+RG=ollama-rg
+LOC=eastus
+VM=ollama-vm
+USER=azureuser
+IMAGE=Ubuntu2204
+SIZE=Standard_D2s_v5
+
+az group create -n $RG -l $LOC
+
+az vm create \
+  -g $RG -n $VM \
+  --image $IMAGE \
+  --size $SIZE \
+  --admin-username $USER \
+  --generate-ssh-keys \
+  --public-ip-sku Standard
+
+# Open HTTP/HTTPS (we’ll reverse-proxy to Ollama)
+az vm open-port -g $RG -n $VM --port 80
+az vm open-port -g $RG -n $VM --port 443   # optional, for TLS later
